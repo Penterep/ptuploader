7e89d1919900c58ea1cf94efb008e1d8f3678687
KakonRaketak <repko.matus@gmail.com>
2026-03-23 16:35:07 +0100

    Task PT-958,959 initial commit: add ptuploader tool with versioning, license, and README

---
+++ README.md
@@ -0,0 +1,101 @@
+[![penterepTools](https://www.penterep.com/external/penterepToolsLogo.png)](https://www.penterep.com/)
+
+
+## PTUPLOADER - The Tool for testing uploads in web apps
+
+
+## Installation
+
+```
+pip install ptuploader
+```
+
+## Adding to PATH
+If you're unable to invoke the script from your terminal, it's likely because it's not included in your PATH. You can resolve this issue by executing the following commands, depending on the shell you're using:
+
+For Bash Users
+```bash
+echo "export PATH=\"`python3 -m site --user-base`/bin:\$PATH\"" >> ~/.bashrc
+source ~/.bashrc
+```
+
+For ZSH Users
+```bash
+echo "export PATH=\"`python3 -m site --user-base`/bin:\$PATH\"" >> ~/.zshrc
+source ~/.zshrc
+```
+
+## Usage examples
+```
+PTUPLOADER -u http://example.com/upload.php -r requestfile.txt -P file -s http://www.example.com/uploads/ -ts EXT
+```
+
+## Options
+```
+-v    --version                       Show script version and exit
+-h    --help                          Show this help message and exit
+-j    --json                          Output in JSON format
+-vv   --verbose                       Enable verbose mode
+
+-ua   --user-agent   <user-agent>     Set User-Agent header
+-c    --cookie       <cookie>         Set Cookie(s)
+-t    --threads      <threads>        Set thread count (default: 10)
+-H    --headers      <header:value>   Set custom header(s)
+-p    --proxy        <proxy>          Set Proxy
+
+-u    --url          <url>            Target upload URL
+-f    --file         <filename>       File to upload
+-sz   --size         <size>           Size of uploaded file
+-n    --number       <number>         Number of uploaded files
+-e    --extensions   <extensions>     Extensions of uploaded files
+-l    --language     <language>       Target language (PHP, ASP, JSP, NET, PY, JS)
+
+-r    --request      <request>        Raw request file or base64 request (headers included)
+-d    --data         <data>           Custom request data
+-P    --parameter    <parameter>      Parameter to test (e.g. file, upload, POST param)
+
+-s    --storage      <url_to_dir>     URL to uploaded files directory
+-sy   --string-yes   <string>         Required string in response for success
+-sn   --string-no    <string>         Forbidden string in response for success
+
+-ts   --tests        <test>           Select test type:
+                                     ANTIVIR     Detect antivirus presence
+                                     MAXSIZE     Max file size limit
+                                     COUNT       Max file count limit
+                                     EXT         Allowed extensions (+ execution test)
+                                     CHARS       Allowed filename characters
+                                     EXEC        Execution bypass techniques
+                                     ADS         Alternate Data Streams
+                                     TRAVERSAL   Path traversal vulnerability
+                                     CONTENT     File content validation
+                                     CT          Content-Type validation
+                                     XXE         XXE vulnerability
+                                     ZIPBOMB     Zip bomb vulnerability
+```
+
+## Dependencies
+```
+ptlibs
+```
+
+## External tools
+None at this moment.
+
+
+## License
+
+Copyright (c) 2025 Penterep Security s.r.o.
+
+ptuploader is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+ptuploader is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with ptuploader. If not, see https://www.gnu.org/licenses/.
+
+## Warning
+
+You are only allowed to run the tool against the websites which
+you have been given permission to pentest. We do not accept any
+responsibility for any damage/harm that this application causes to your
+computer, or your network. Penterep is not responsible for any illegal
+or malicious use of this code. Be Ethical!
---
+++ ptuploader/ptuploader.py
@@ -0,0 +1,257 @@
+#!/usr/bin/python3
+"""
+Copyright (c) 2025 Penterep Security s.r.o.
+
+ptuploader is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ptuploader is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ptuploader.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import argparse
+import importlib
+import os
+import threading
+import sys; sys.path.append(__file__.rsplit("/", 1)[0])
+
+from types import ModuleType
+
+from ptlibs import ptjsonlib, ptmisclib, ptnethelper
+from ptlibs.ptprinthelper import ptprint, print_banner, help_print, get_colored_text
+from ptlibs.threads import ptthreads, printlock
+from ptlibs.http.http_client import HttpClient
+
+from _version import __version__
+
+
+class PtUploader:
+    def __init__(self, args):
+        self.ptjsonlib   = ptjsonlib.PtJsonLib()
+        self.ptthreads   = ptthreads.PtThreads()
+        self._lock       = threading.Lock()
+        self.args        = args
+        self.http_client = HttpClient(args=self.args, ptjsonlib=self.ptjsonlib)
+
+    def fetch(self, url, allow_redirects=False):
+        try:
+            return self.http_client.send_request(
+                url=url,
+                method="GET",
+                headers=self.args.headers,
+                allow_redirects=allow_redirects,
+                timeout=self.args.timeout,
+            )
+        except Exception:
+            return None
+
+    def run(self) -> None:
+        """Main method"""
+        tests = self.args.tests or _get_all_available_modules()
+        self.ptthreads.threads(tests, self.run_single_module, self.args.threads)
+
+        self.ptjsonlib.set_status("finished")
+        ptprint(self.ptjsonlib.get_result_json(), "", self.args.json)
+
+    def run_single_module(self, module_name: str) -> None:
+        """Safely loads and executes a specified module's `run()` function.
+
+        The method locates the module file in the "modules" directory, imports it dynamically,
+        and executes its `run()` method with provided arguments and a shared `ptjsonlib` object.
+        Each module call receives a dedicated PrintLock for thread-safe, atomic output.
+
+        If the module or its `run()` method is missing, or if an error occurs during execution,
+        it logs appropriate messages to the user.
+
+        Args:
+            module_name (str): The name of the module (without `.py` extension) to execute.
+        """
+        try:
+            with self._lock:
+                module = _import_module_from_path(module_name)
+
+            if hasattr(module, "run") and callable(module.run):
+                print_lock = printlock.PrintLock()
+                try:
+                    module.run(
+                        args=self.args,
+                        ptjsonlib=self.ptjsonlib,
+                        http_client=self.http_client,
+                        print_lock=print_lock,
+                    )
+
+                except Exception as e:
+                    ptprint(str(e), "ERROR", not self.args.json)
+                finally:
+                    print_lock.lock_print_output(condition=not self.args.json)
+            else:
+                ptprint(f"Module '{module_name}' does not have 'run' function", "WARNING", not self.args.json)
+
+        except FileNotFoundError:
+            ptprint(f"Module '{module_name}' not found", "ERROR", not self.args.json)
+        except Exception as e:
+            ptprint(f"Error running module '{module_name}': {e}", "ERROR", not self.args.json)
+
+
+def _import_module_from_path(module_name: str) -> ModuleType:
+    """
+    Dynamically imports a Python module from a given file path.
+
+    Args:
+        module_name (str): Name under which to register the module.
+
+    Returns:
+        ModuleType: The loaded Python module object.
+
+    Raises:
+        ImportError: If the module cannot be found or loaded.
+    """
+    module_path = os.path.join(os.path.dirname(__file__), "modules", f"{module_name}.py")
+
+    spec = importlib.util.spec_from_file_location(module_name, module_path)
+    if spec is None:
+        raise ImportError(f"Cannot find spec for {module_name} at {module_path}")
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+    spec.loader.exec_module(module)
+    return module
+
+
+def _get_all_available_modules() -> list:
+    """
+    Returns a list of available Python module names from the 'modules' directory.
+
+    Modules must:
+    - Not start with an underscore
+    - Have a '.py' extension
+    """
+    modules_folder = os.path.join(os.path.dirname(__file__), "modules")
+    available_modules = [
+        f.rsplit(".py", 1)[0]
+        for f in sorted(os.listdir(modules_folder))
+        if f.endswith(".py") and not f.startswith("_")
+    ]
+    return available_modules
+
+
+def get_help():
+    """
+    Generate structured help content for the CLI tool.
+
+    Returns:
+        list: A list of dictionaries representing sections of help content.
+    """
+
+    def _get_available_modules_help() -> list:
+        rows = []
+        available_modules = _get_all_available_modules()
+        for module in available_modules:
+            mod = _import_module_from_path(module)
+            label = getattr(mod, "__TESTLABEL__", f"Test for {module.upper()}")
+            row = ["", "", f" {module.upper()}", label.rstrip(':')]
+            rows.append(row)
+        return sorted(rows, key=lambda x: x[2])
+
+    return [
+        {"description": ["Tool for testing file upload vulnerabilities on web servers"]},
+        {"usage": ["ptuploader <options>"]},
+        {"usage_example": [
+            "ptuploader -u https://www.example.com/upload -f file.php",
+            "ptuploader -u https://www.example.com/upload -f file.php -ts EXT EXEC",
+            "ptuploader -u https://www.example.com/upload -r request.txt -s http://example.com/uploads/",
+        ]},
+        {"options": [
+            ["-u",  "--url",          "<url>",            "Tested URL"],
+            ["-f",  "--file",         "<filename>",       "File to upload"],
+            ["-sz", "--size",         "<size>",           "Size of uploaded file"],
+            ["-n",  "--number",       "<number>",         "Number of uploaded files"],
+            ["-e",  "--extensions",   "<extensions>",     "Set extensions of uploaded file"],
+            ["-l",  "--language",     "<language>",       "Set app programming language (PHP, ASP, JSP, NET, PY, JS)"],
+            ["-r",  "--request",      "<request>",        "Set request file or content of request in base64 (headers included)"],
+            ["-d",  "--data",         "<data>",           "Set request-data"],
+            ["-P",  "--parameter",    "<parameter>",      "Set parameter to test (e.g. GET, POST parameters)"],
+            ["-s",  "--storage",      "<url_to_dir>",     "Set URL to directory with upload (http://example.com/uploads/)"],
+            ["-sy", "--string-yes",   "<string>",         "Set the string that must be included in the response to pass the success test"],
+            ["-sn", "--string-no",    "<string>",         "Set the string that must not be included in the response to pass the success test"],
+            ["-ts", "--tests",        "<test>",           "Specify one or more tests to perform:"],
+            *_get_available_modules_help(),
+            ["", "", "", ""],
+            ["-ua", "--user-agent",   "<user-agent>",     "Set User-Agent header"],
+            ["-c",  "--cookie",       "<cookie>",         "Set Cookie(s)"],
+            ["-H",  "--headers",      "<header:value>",   "Set custom header(s)"],
+            ["-p",  "--proxy",        "<proxy>",          "Set Proxy"],
+            ["-t",  "--threads",      "<threads>",        "Set thread count (default 10)"],
+            ["-vv", "--verbose",      "",                 "Enable verbose mode"],
+            ["-v",  "--version",      "",                 "Show script version and exit"],
+            ["-h",  "--help",         "",                 "Show this help message and exit"],
+            ["-j",  "--json",         "",                 "Output in JSON format"],
+        ]}
+    ]
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(add_help=False, description=f"{SCRIPTNAME} <options>")
+
+    # Target
+    parser.add_argument("-u",  "--url",          type=str, required=True)
+    parser.add_argument("-f",  "--file",         type=str, default=None)
+    parser.add_argument("-sz", "--size",         type=str, default=None)
+    parser.add_argument("-n",  "--number",       type=int, default=None)
+    parser.add_argument("-e",  "--extensions",   type=str, nargs="+", default=None)
+    parser.add_argument("-l",  "--language",     type=lambda s: s.upper(), default=None,
+                        choices=["PHP", "ASP", "JSP", "NET", "PY", "JS"])
+    parser.add_argument("-r",  "--request",      type=str, default=None)
+    parser.add_argument("-d",  "--data",         type=str, default=None)
+    parser.add_argument("-P",  "--parameter",    type=str, default=None)
+    parser.add_argument("-s",  "--storage",      type=str, default=None)
+    parser.add_argument("-sy", "--string-yes",   type=str, default=None, dest="string_yes")
+    parser.add_argument("-sn", "--string-no",    type=str, default=None, dest="string_no")
+
+    # Tests
+    parser.add_argument("-ts", "--tests",        type=lambda s: s.upper(), nargs="+", default=None,
+                        choices=["ANTIVIR", "MAXSIZE", "COUNT", "EXT", "CHARS", "EXEC",
+                                 "ADS", "TRAVERSAL", "CONTENT", "CT", "XXE", "ZIPBOMB"])
+
+    # HTTP options
+    parser.add_argument("-ua", "--user-agent",   type=str, default=None, dest="user_agent")
+    parser.add_argument("-c",  "--cookie",       type=str, default=None)
+    parser.add_argument("-H",  "--headers",      type=str, nargs="+", default=None)
+    parser.add_argument("-p",  "--proxy",        type=str, default=None)
+
+    # General options
+    parser.add_argument("-t",  "--threads",      type=int, default=10)
+    parser.add_argument("-vv", "--verbose",      action="store_true")
+    parser.add_argument("-j",  "--json",         action="store_true")
+    parser.add_argument("-v",  "--version",      action="version", version=f"{SCRIPTNAME} {__version__}")
+
+    parser.add_argument("--socket-address",      type=str, default=None)
+    parser.add_argument("--socket-port",         type=str, default=None)
+    parser.add_argument("--process-ident",       type=str, default=None)
+
+    if len(sys.argv) == 1 or "-h" in sys.argv or "--help" in sys.argv:
+        ptprint(help_print(get_help(), SCRIPTNAME, __version__))
+        sys.exit(0)
+
+    args = parser.parse_args()
+
+    print_banner(SCRIPTNAME, __version__, args.json, 0)
+    return args
+
+
+def main():
+    global SCRIPTNAME
+    SCRIPTNAME = os.path.splitext(os.path.basename(__file__))[0]
+    args = parse_args()
+    script = PtUploader(args)
+    script.run()
+
+
+if __name__ == "__main__":
+    main()
